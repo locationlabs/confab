910753f74c9a1cf10fbb1ccf9f4994dd06476798
Jesse Myers <jesse@locationlabs.com>
2012-10-03 13:34:17 -0700

    Initial vanilla skeleton.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='confab',
+      version='0.2',
+      description='Configuration management with Fabric and Jinja.',
+      author='Location Labs',
+      author_email='info@locationlabs.com',
+      url='http://github.com/locationlabs/confab',
+      packages=find_packages(exclude=['*.tests']),
+      namespace_packages=[
+        'confab'
+        ],
+      setup_requires=[
+        'nose>=1.0'
+        ],
+      install_requires=[
+        'Fabric>=1.4',
+        'Jinja'
+        ],
+      )
+
