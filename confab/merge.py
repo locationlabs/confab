97e5251e0d1e14911fcfcc8072b5cb786f00e0e8
Jesse Myers <jesse@locationlabs.com>
2012-10-03 14:16:37 -0700

    Add merge and tests.

---
+++ confab/merge.py
@@ -0,0 +1,92 @@
+"""
+Configuration data structure merging functions.
+
+We expect to obtain hierarchical configurations as native dictionaries
+and want to merge the higher precedence (override) data into the
+lower precedence (default) data, potentially multiple times.
+
+Merge rules are as follows:
+ - Dictionaries are merged recursively by default.
+ - Lists and primitives are replaced by default.
+ - Callables are called to provide custom extension.
+
+For example, a list of hosts in the default dictionary will normally
+be replaced by values defined in the override dictionary; however if
+the override dictionary's list is a callable, it can be made to do
+something else, such as append a new host to the default list.
+"""
+
+def _best(default_value, override_value):
+    """
+    Return the best value according the merge rules.
+    """
+    if callable(override_value):
+        # custom callable
+        return override_value(default_value)
+    elif isinstance(default_value, dict) and isinstance(override_value, dict):
+        # merge recursively
+        return _merge(default_value,override_value)
+    elif override_value:
+        # replace with override
+        return override_value
+    else:
+        return default_value
+
+def _iterkeys(default, override):
+    """
+    Return an iterator over the keys of the both dicts.
+    """
+    return iter(set(default.keys()) | set(override.keys()))
+
+def _entry(default, override, key):
+    """
+    Return a dictionary entry with the best value from the default and 
+    override dictionaries.
+    """
+    return (key, _best(default.get(key), override.get(key)))
+
+def _merge(default, override):
+    """Recursively merge two dictionaries.
+    
+    """
+    return dict(_entry(default, override, key) for key in _iterkeys(default, override))
+
+def merge(*args):
+    """Recursively merge multiple dictionaries.
+    
+    """
+
+    merged = args[0] if len(args) > 0 else {}
+    for override in args[1:]:
+        merged = _merge(merged,override)
+    return merged
+    
+ 
+class Append(list):
+    """
+    Customized callable list that appends its values to the default.
+    """
+    def __init__(self, *args):
+        super(Append,self).__init__(args)
+
+    def __call__(self, default):
+        return default + self
+
+class Prepend(list):
+    """
+    Customized callable list that prepends its values to the default.
+    """
+
+    def __init__(self, *args):
+        super(Prepend,self).__init__(args)
+
+    def __call__(self, default):
+        return self + default
+
+
+def append(*args):
+    return Append(*args)
+
+def prepend(*args):
+    return Prepend(*args)
+
---
+++ confab/tests/test_merge.py
@@ -0,0 +1,87 @@
+from confab.merge import merge, append, prepend
+
+from unittest import TestCase
+
+class TestMerge(TestCase):
+
+    def test_no_inputs(self):
+        """
+        Sane results if no inputs are provided.
+        """
+
+        merged = merge()
+
+        self.assertEquals(merged, {})
+
+
+    def test_no_overrride(self):
+        """
+        Sane results if no overrides are provided.
+        """
+
+        default = {
+            'foo' : 'bar'
+            }
+
+        merged = merge(default)
+
+        self.assertEquals(merged, default)
+    
+    def test_override(self):
+        """
+        Overrides work as expected.
+        """
+
+        default = {
+            'value1': 'foo',
+            'list': [ 'foo' ],
+            'dict': {
+                'value2': 'foo',
+                'value3': 'foo'
+                },
+            }
+
+        override = {
+            'value1': 'bar',
+            'list': [ 'bar' ],
+            'dict': {
+                'value2': 'bar',
+                'value4': 'bar'
+                },
+            }
+
+        merged = merge(default, override)
+
+        # primitive values are replaced
+        self.assertEqual('bar', merged['value1'])
+
+        # lists are replaced
+        self.assertEqual(['bar'], merged['list'])
+
+        # dictionaries are recursively merged
+        self.assertEqual('bar', merged['dict']['value2'])
+        self.assertEqual('foo', merged['dict']['value3'])
+        self.assertEqual('bar', merged['dict']['value4'])
+
+        
+    def test_override_custom(self):
+        """
+        Custom append() and prepend() lists append and prepend to
+        the origin using N-ary merge.
+        """
+
+        default = {
+            'list': [ 'two' ]
+            }
+
+        override1 = {
+            'list': append('three', 'four')
+            }
+
+        override2 = {
+            'list': prepend('one')
+            }
+
+        merged = merge(default, override1, override2)
+
+        self.assertEquals(merged['list'], ['one', 'two', 'three', 'four'])
