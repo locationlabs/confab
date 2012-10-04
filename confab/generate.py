2563d9c0b42e488091d20dd51dcc452732a5b517
Jesse Myers <jesse@locationlabs.com>
2012-10-03 20:18:49 -0700

    Improve configurability of generation routine.

--- confab/generate.py
+++ confab/generate.py
@@ -5,6 +5,10 @@ We expect as input a Jinja2 Environment, which must use a Loader
 that supports list_templates(). Use of the Environment abstraction
 nicely separates the concern of which templates to render from
 that of rendering them.
+
+Users can provide two further customizations:
+ - a filter_func that limits which templates are returned by list_templates()
+ - a mime_type_func that limits which templates are treated as templates
 """
 
 import os
@@ -31,18 +35,19 @@ def _render_string(source, env, data):
     """
     return env.from_string(source).render(**data)
 
-def _is_temporary(file_name):
+def _is_template(mime_type):
     """
-    Determine if a file is a temporary file.
+    Should a template of this mime type be treated as a template (instead of a verbatim file)?
     """
-    return file_name.endswith('~')
+    return mime_type.split('/')[0] == 'text' or mime_type == 'application/xml'
+
 
 class TemplateWriter(object):
     """
     Helper class that encapsulates writing out a single template.
     """
 
-    def __init__(self, env, template, data, dest_dir):
+    def __init__(self, env, template, data, dest_dir, mime_type_func):
         self.env = env
         self.template = template
         self.data = data
@@ -50,6 +55,7 @@ class TemplateWriter(object):
         self.dest_file_name = _render_string(os.sep.join((dest_dir,self.template.name)),
                                              self.env,
                                              self.data)
+        self.mime_type_func = mime_type_func if mime_type_func else _is_template
 
     def _write_verbatim(self):
         """
@@ -59,17 +65,11 @@ class TemplateWriter(object):
 
     def _write_template(self):
         """
-        Write the template file to the destination, preserving ownerships.
+        Write the template file to the destination, preserving ownership.
         """
         with open(self.dest_file_name, 'w') as dest_file:
             dest_file.write(self.template.render(**self.data) + '\n')
             shutil.copystat(self.template.filename, self.dest_file_name)
-
-    def _is_template(self):
-        """
-        Should this template be treated as a template (instead of a verbatim file)?
-        """
-        return self.mime_type.split('/')[0] == 'text' or self.mime_type == 'application/xml'
 
     def write(self):
         """
@@ -79,33 +79,33 @@ class TemplateWriter(object):
         # ensure that destination directory exists
         _ensure_dir(os.path.dirname(self.dest_file_name))
 
-        if self._is_template():
+        if self.mime_type_func(self.mime_type):
             self._write_template()
         else:
             self._write_verbatim()
 
 
-def generate(env, data, dest_dir):
+def generate(env, data, dest_dir, filter_func=None, mime_type_func=None):
     """
     Generate templated outputs by listing all available templates in
     the Jinja2 Environment and writing them out.
+
+    Callers may optionally provide a filter_func, e.g. to omit temporary
+    files or files that match an unexpected pattern.
     """
 
-    # Ensure that destination is exists and is empty
+    # Ensure that destination exists and is empty
     _clear_dir(dest_dir)
     _ensure_dir(dest_dir)
 
     def iter_writers():
-        for template_name in env.list_templates():
-            if not _is_temporary(template_name):
-                yield TemplateWriter(env,
-                                     env.get_template(template_name),
-                                     data,
-                                     dest_dir)
+        for template_name in env.list_templates(filter_func=filter_func):
+            yield TemplateWriter(env,
+                                 env.get_template(template_name),
+                                 data,
+                                 dest_dir,
+                                 mime_type_func)
 
     # Write all templates
     for writer in iter_writers():
         writer.write()
-
-
-
--- confab/tests/test_generate.py
+++ confab/tests/test_generate.py
@@ -1,6 +1,6 @@
 from confab.generate import generate
 from unittest import TestCase
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, PackageLoader, StrictUndefined, UndefinedError
 
 import os
 import shutil
@@ -18,25 +18,72 @@ class TempDir(object):
     def __exit__(self, exc_type, value, traceback):
         shutil.rmtree(self.path)
 
+def _make_env():
+    return Environment(loader=PackageLoader('confab.tests'),
+                       undefined=StrictUndefined)
+
 class TestGenerate(TestCase):
+    
+    def _read_value(self, tmp_dir, file_name):
+        return open(os.sep.join((tmp_dir,file_name))).read().strip()
 
-    def test_environment(self):
+    def test_undefined(self):
+        """
+        An exception is raised if a template value is undefined.
+        """
+        env = _make_env()
+        data = {}
 
-        env = Environment(loader=PackageLoader('confab.tests'))
+        with self.assertRaises(UndefinedError):
+            with TempDir() as tmp_dir:
+                generate(env, data, tmp_dir)
+
+    def test_generate(self):
+        """
+        Generated templates have the correct values.
+        """
+        env = _make_env()
         data = {'foo':'foo', 'bar':'bar'}
 
         with TempDir() as tmp_dir:
             generate(env, data, tmp_dir)
 
             # foo.txt is populated with 'foo'
-            self.assertEqual(
-                open(os.sep.join((tmp_dir,'foo.txt'))).read().strip(),
-                'foo'
-                )
+            self.assertEquals('foo', self._read_value(tmp_dir,'foo.txt'))
 
             # bar.txt is populated with 'bar' and path is substituted
-            self.assertEqual(
-                open(os.sep.join((tmp_dir,'bar/bar.txt'))).read().strip(),
-                'bar'
-                )
-            
+            self.assertEquals('bar', self._read_value(tmp_dir,'bar/bar.txt'))
+                    
+    def test_filter_func(self):
+        """
+        Passing a filter_func limits which templates are generated.
+        """
+        env = _make_env()
+        data = {'foo':'foo', 'bar':'bar'}
+
+        def filter_func(file_name):
+            return file_name != 'foo.txt'
+
+        with TempDir() as tmp_dir:
+            generate(env, data, tmp_dir, filter_func=filter_func)
+                    
+            # not generated
+            with self.assertRaises(IOError):
+                self._read_value(tmp_dir,'foo.txt')
+
+    def test_mime_type_func(self):
+        """
+        Passing a mime_type_func controls whether templates are rendered.
+        """
+        env = _make_env()
+        data = {'foo':'foo', 'bar':'bar'}
+
+        def mime_type_func(file_name):
+            return False
+
+        with TempDir() as tmp_dir:
+            generate(env, data, tmp_dir, mime_type_func=mime_type_func)
+
+            # templates not rendered (though paths are)
+            self.assertEquals('{{foo}}', self._read_value(tmp_dir,'foo.txt'))
+            self.assertEquals('{{bar}}', self._read_value(tmp_dir,'bar/bar.txt'))
