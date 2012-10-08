4c2b4d10beac508747364680d9e9a5d7c3488f97
Jesse Myers <jesse@locationlabs.com>
2012-10-08 10:37:05 -0700

    Add loaders and ConfFiles model to public API.

--- confab/api.py
+++ confab/api.py
@@ -2,6 +2,16 @@
 Non-init module for doing convenient * imports from.
 """
 
+# core
+from confab.conffiles import ConfFiles
+
+# jinja2 environment loading
+from confab.loaders import load_environment_from_dir, load_environment_from_package
+
+# data loading
+from confab.data import load_data_from_dir
+
+# fabric tasks
 from confab.diff import diff
 from confab.generate import generate
 from confab.pull import pull
