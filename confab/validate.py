b18f047e4f7fc5ba0f2c03d179f52eebf8ceaade
Jesse Myers <jesse@locationlabs.com>
2012-11-21 16:58:26 -0800

    Improve argument validation. Provide directory string checked if provided.

--- confab/validate.py
+++ confab/validate.py
@@ -10,32 +10,40 @@ def validate_templates_dir(templates_dir):
     """
     Template directory must be defined and exist.
     """
-    if not templates_dir or not os.path.isdir(templates_dir):
-        abort('Please provide a valid templates_dir')
+    if not templates_dir:
+        abort('Please provide a valid templates directory')
+    elif not os.path.isdir(templates_dir):
+        abort('{templates_dir} is not a valid directory'.format(templates_dir=templates_dir))
 
 
 def validate_data_dir(data_dir):
     """
     Data directory must be defined and exist.
     """
-    if not data_dir or not os.path.isdir(data_dir):
+    if not data_dir:
         abort('Please provide a valid data_dir')
+    elif not os.path.isdir(data_dir):
+        abort('{data_dir} is not a valid directory'.format(data_dir=data_dir))
 
 
 def validate_generated_dir(generated_dir):
     """
     Generated directory must be defined and not be a regular file.
     """
-    if not generated_dir or (os.path.exists(generated_dir) and not os.path.isdir(generated_dir)):
+    if not generated_dir:
         abort('Please provide a valid generated_dir')
+    elif os.path.exists(generated_dir) and not os.path.isdir(generated_dir):
+        abort('{generated_dir} is not a valid directory'.format(generated_dir=generated_dir))
 
 
 def validate_remotes_dir(remotes_dir):
     """
     Remotes directory must be defined and not be a regular file.
     """
-    if not remotes_dir or (os.path.exists(remotes_dir) and not os.path.isdir(remotes_dir)):
+    if not remotes_dir:
         abort('Please provide a valid remotes_dir')
+    elif os.path.exists(remotes_dir) and not os.path.isdir(remotes_dir):
+        abort('{remotes_dir} is not a valid directory'.format(remotes_dir=remotes_dir))
 
 
 def validate_host():
